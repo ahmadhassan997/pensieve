5eddd4a16daaf30881939a48986186b3eaf4ac6c
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-12-31 17:27:50 -0600

    wip: change dash bitrate and resolution settings

--- rl_server/simple_server.py
+++ rl_server/simple_server.py
@@ -12,14 +12,14 @@ from collections import deque
 import numpy as np
 import time
 
-VIDEO_BIT_RATE = [300, 750, 1200, 1850, 2850, 4300, 6144, 17408]  # Kbps
-BITRATE_REWARD = [1, 2, 3, 12, 15, 20, 25, 30]
-BITRATE_REWARD_MAP = {0: 0, 95: 1, 150: 2, 276: 3, 750: 12, 2048: 15, 4096: 20, 6144: 25, 17408: 30}
+VIDEO_BIT_RATE = [1500, 4900, 8200, 11700, 32800, 152400]  # Kbps
+BITRATE_REWARD = [1, 2, 3, 12, 15, 20]
+BITRATE_REWARD_MAP = {0: 0, 1500: 1, 4900: 2, 8200: 3, 11700: 12, 32800: 15, 152400: 20}
 M_IN_K = 1000.0
 DEFAULT_QUALITY = 0  # default video quality without agent
 REBUF_PENALTY = 4.3  # 1 sec rebuffering -> this number of Mbps
 SMOOTH_PENALTY = 1
-TOTAL_VIDEO_CHUNKS = 59
+TOTAL_VIDEO_CHUNKS = 120
 SUMMARY_DIR = './results'
 LOG_FILE = './results/log'
 
--- run_exp/run_all_traces.py
+++ run_exp/run_all_traces.py
@@ -47,31 +47,41 @@ PROCESS_ID = 7
 command_RL = 'python run_traces.py ' + TRACE_PATH + ' ' + ABR_ALGO + ' ' + str(PROCESS_ID) + ' ' + ip
 
 # proc_BB = subprocess.Popen(command_BB,
-#                            # stdout=subprocess.PIPE,
+#                            stdout=subprocess.PIPE,
 #                            shell=True)
 # time.sleep(0.1)
-# proc_RB = subprocess.Popen(command_RB, stdout=subprocess.PIPE, shell=True)
-# time.sleep(0.1)
-# proc_FIXED = subprocess.Popen(command_FIXED, stdout=subprocess.PIPE, shell=True)
-# time.sleep(0.1)
-# proc_FESTIVE = subprocess.Popen(command_FESTIVE, stdout=subprocess.PIPE, shell=True)
-# time.sleep(0.1)
-# proc_BOLA = subprocess.Popen(command_BOLA, stdout=subprocess.PIPE, shell=True)
-# time.sleep(0.1)
-proc_fastMPC = subprocess.Popen(command_fastMPC,
-                                stdout=subprocess.PIPE,
-                                shell=True)
+proc_RB = subprocess.Popen(command_RB,
+                           stdout=subprocess.PIPE,
+                           shell=True)
 time.sleep(0.1)
-# proc_robustMPC = subprocess.Popen(command_robustMPC, stdout=subprocess.PIPE, shell=True)
+# proc_FIXED = subprocess.Popen(command_FIXED,
+#                               stdout=subprocess.PIPE,
+#                               shell=True)
+# time.sleep(0.1)
+# proc_FESTIVE = subprocess.Popen(command_FESTIVE,
+#                                 stdout=subprocess.PIPE,
+#                                 shell=True)
+# time.sleep(0.1)
+# proc_BOLA = subprocess.Popen(command_BOLA,
+#                              stdout=subprocess.PIPE,
+#                              shell=True)
+# time.sleep(0.1)
+# proc_fastMPC = subprocess.Popen(command_fastMPC,
+#                                 stdout=subprocess.PIPE,
+#                                 shell=True)
+# time.sleep(0.1)
+# proc_robustMPC = subprocess.Popen(command_robustMPC,
+#                                   stdout=subprocess.PIPE,
+#                                   shell=True)
 # time.sleep(0.1)
 # proc_RL = subprocess.Popen(command_RL, stdout=subprocess.PIPE, shell=True)
 # time.sleep(0.1)
 
 # proc_BB.wait()
-# proc_RB.wait()
+proc_RB.wait()
 # proc_FIXED.wait()
 # proc_FESTIVE.wait()
 # proc_BOLA.wait()
-proc_fastMPC.wait()
+# proc_fastMPC.wait()
 # proc_robustMPC.wait()
 # proc_RL.wait()
